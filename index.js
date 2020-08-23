670e76c2ec7b4f7aa9d996c3e3b7eb7cbd36eff4
FigmentBoy <17240592+FigmentBoy@users.noreply.github.com>
2020-08-23 14:21:19 -0500

    Added bin

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-if (require('fs').readFileSync('setup.txt').indexOf('true') != -1) {
-    require( "child_process" ).spawnSync( "setup.bat" , { stdio: "inherit", stdin: "inherit" } );
-    require('fs').writeFileSync('setup.txt', 'false')
-    console.clear()
+module.exports = () => {
+    if (require('fs').readFileSync('setup.txt').indexOf('true') != -1) {
+        require( "child_process" ).spawnSync( "setup.bat" , { stdio: "inherit", stdin: "inherit" } );
+        require('fs').writeFileSync('setup.txt', 'false')
+        console.clear()
+    }
+    require( "child_process" ).spawnSync( "run.bat" , { stdio: "inherit", stdin: "inherit" } );
 }
-require( "child_process" ).spawnSync( "run.bat" , { stdio: "inherit", stdin: "inherit" } );
