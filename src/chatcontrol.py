0618d816605885845bcfa9493b1c4ae7446cc38a
Figment <17240592+FigmentBoy@users.noreply.github.com>
2020-08-23 17:13:50 -0500

    Update chatcontrol.py

--- src/chatcontrol.py
+++ src/chatcontrol.py
@@ -19,7 +19,7 @@ def hold():
 def release():
     win32api.SendMessage(win, win32con.WM_LBUTTONUP, win32con.MK_LBUTTON, win32api.MAKELONG(10, 10))
 
-OAUTHSITE = 'https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=ifn3ztal79zhqkclxtmu0oljs1su98&redirect_uri=https://twitchapps.com/tokengen/&scope=chat:read chat:edit'
+OAUTHSITE = 'https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=ifn3ztal79zhqkclxtmu0oljs1su98&redirect_uri=https://twitchapps.com/tokengen/&scope=chat:read'
 
 configf = open('src/config.json', 'r')
 config = json.loads(configf.read())
@@ -59,37 +59,5 @@ with Observer('GDControl', oauth) as observer:
                         hold()
                     elif event.message.lower()=='!release' and mem.percent > 0:
                         release()
-                    elif event.message.lower() == '!rick' and event.nickname.lower() == 'figmentboy':
-                        mixer.music.stop()
-                        mixer.music.load('src/sounds/rick.mp3')
-                        mixer.music.play()
-                    elif event.message.lower() == '!bwomp' and event.nickname.lower() == 'figmentboy':
-                        mixer.music.stop()
-                        mixer.music.load('src/sounds/bwomp.mp3')
-                        mixer.music.play()
-                    elif event.message.lower() == '!knock' and event.nickname.lower() == 'figmentboy':
-                        mixer.music.stop()
-                        mixer.music.load('src/sounds/knock.mp3')
-                        mixer.music.play()
-                    elif event.message.lower() == '!stopsounds' and event.nickname.lower() == 'figmentboy':
-                        mixer.music.stop()
-                    elif event.message.lower().startswith('!speed') and event.nickname.lower() == 'figmentboy':
-                        mem.set_speed_value(float(event.message[7:]))
-                    elif event.message.lower() == '!kill' and event.nickname.lower() == 'figmentboy':
-                        mem.player_kill()
-                    elif event.message.lower() == '!stop' and event.nickname.lower() == 'figmentboy':
-                        mem.player_freeze()
-                    elif event.message.lower() == '!start' and event.nickname.lower() == 'figmentboy':
-                        mem.player_unfreeze()
-                    elif event.message.lower().startswith('!eval') and event.nickname.lower() == 'figmentboy':
-                        try:
-                            eval(event.message[6:])
-                        except:
-                            observer.send_message(SyntaxError, channel)
-                            pass
-                        
-
-
-        except KeyboardInterrupt:
-            observer.leave_channel(channel)
-            break
+        except:
+            mem = gd.memory.get_memory()
